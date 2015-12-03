cc41c071dff9ce27c4783e3a94804072c4e98175
ishiiy93 <ishiiy@uw.edu>
2015-12-02 23:10:47 -0800

    Updated with comments for classroom demo

--- js/disqus.js
+++ js/disqus.js
@@ -1,28 +1,15 @@
-// Enter Access Token
-var ACCESS_TOKEN =  
-
-// Enter API Key
-var API_KEY = 
-
 // creates the angular app
 var myApp = angular.module('myApp', []);
-
-
 
 myApp.controller('myCtrl', function($scope, $http) {
   $scope.posts = {};
 
-  $http.get('https://disqus.com/api/3.0/forums/listPosts.json', {
-    params: {
-      access_token: ACCESS_TOKEN,
-      api_key: API_KEY,
-      forum: "youtatest1",
-      order: "asc" 
-    } 
-  })
-
-  .success(function(response) {
-    var data = response.response;
-    $scope.posts = data;
-  });
-});
+  // Create a $http.get with the link "https://disqus.com/api/3.0/forums/listPosts.json"
+  // Within, have the following parameters:
+  // access_token: This is the secret key
+  // api_key: This is the public key
+  // forum: This is the site name "youtatest1"
+  // order: this is the order in which your comments show up in the thread. Choose "asc" to show oldest at the top
+  
+  //Once the http get is successful, insert the json response into a angular scope variable called "posts"
+  
